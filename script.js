7369105d203623d7995f08e585121101f8498ca3
Jennifer Henry <jenhenry1995@gmail.com>
2020-09-23 16:03:19 -0700

    created scoreboard

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 var start = document.getElementById('startButton');
 var quiz = document.getElementById('quiz');
 var question = document.getElementById('question');
@@ -9,6 +6,10 @@ var choice_B = document.getElementById('B');
 var choice_C = document.getElementById('C');
 var choice_D = document.getElementById('D');
 var result = document.getElementById('result');
+var score = document.getElementById('score');
+var inputName = document.getElementById('inputName');
+var submit = document.getElementById('submit');
+var scoreBoard = document.getElementById('scoreBoard');
 
 
 
@@ -16,6 +17,7 @@ var correct = 0;
 var correctResponse = "correct!";
 var incorrect = 0;
 var incorrectResponse = 'incorrect!';
+
 
 
 //questions are placed into an array wiht objects
@@ -27,16 +29,16 @@ var allQuestions = [
         choiceC: "Steve Miller Band",
         choiceD: "Aerosmith",
 
-        Correct: "Aerosmith",
+        Correct: "Steve Miller Band",
     },
     {   //question 2
         question: "2. Who sings 'Sweet Child of Mine?",  
-        choiceA: "Guns & Roses",
+        choiceA: "Guns N' Roses",
         choiceB: "Metallica",
         choiceC: "Van Halen",
         choiceD: "Foreigner",
 
-        Correct: "Guns & Roses",
+        Correct: "Guns N' Roses",
     },   
     {   //question 3
         question: "3. Who sings 'Don't Stop Believin'?",  
@@ -57,30 +59,41 @@ var allQuestions = [
         Correct: "AC/DC",
     },      
  ]
+
+
  var questionIndex = 0;
 
  //renders the question to the screen with buttons
  function produceQuestion(){
-     let quest = allQuestions[questionIndex];
-    question.innerHTML = quest.question;
-    choice_A.textContent = quest.choiceA;
-    choice_B.textContent = quest.choiceB;
-    choice_C.textContent = quest.choiceC;
-    choice_D.textContent = quest.choiceD;
- } 
-
- function answerChecker(){
-     debugger;
-    if (choice_A == allQuestions[questionIndex][5]){
-        console.log('correct');
-        //result.innerHTML = 'Correct!';
-        correct++;
+    const currentQuestion = allQuestions[questionIndex]; 
+    //when allQuestions[questionIndex] returns as undefined it will go to the else statement :)
+    if(currentQuestion){
+        question.innerHTML = currentQuestion.question;
+        choice_A.innerHTML = currentQuestion.choiceA;
+        choice_B.innerHTML = currentQuestion.choiceB;
+        choice_C.innerHTML = currentQuestion.choiceC;
+        choice_D.innerHTML = currentQuestion.choiceD;
     }else{
-        result.innerHTML = 'Incorrect!'
-        incorrect++;
+        quiz.style.display = 'none';
+        score.style.display = 'block';
+        
+        submit.addEventListener('click', function(){
+            localStorage.setItem(inputName.value, correct);
+            inputName.style.display = 'none';
+            submit.style.display = 'none';
+
+            var keyArray = Object.keys(localStorage);
+            for(var i = 0; i < keyArray.length; i++){
+                var key = keyArray[i];
+                var keyValue = localStorage.getItem(key);
+                var child = document.createElement('div');
+                child.innerHTML = key + ": " +keyValue;
+                scoreBoard.appendChild(child);
+            }
+
+        });
     }
-    questionIndex++;
-}
+ } 
 
 //starts the quiz
 start.addEventListener('click', function(){
@@ -89,23 +102,88 @@ start.addEventListener('click', function(){
     quiz.style.display = 'block';
 } );
 
-choice_A.addEventListener('click', answerChecker());
-choice_B.addEventListener('click', answerChecker());
-choice_C.addEventListener('click', answerChecker());
-choice_D.addEventListener('click', answerChecker());
+
+// function answerChecker(){
+//     debugger;
+//    if (choice_A == allQuestions[questionIndex].correct){
+//        console.log('correct');
+//        //result.innerHTML = 'Correct!';
+//        correct++;
+//    }else{
+//        result.innerHTML = 'Incorrect!'
+//        incorrect++;
+//    }
+//    questionIndex++;
+//    produceQuestion();
+// }
+
+choice_A.addEventListener('click', function(){
+   if (choice_A.innerHTML === allQuestions[questionIndex].Correct){
+       console.log('correct');
+       result.innerHTML = 'Correct!';
+       correct++;
+   }else{
+       result.innerHTML = 'Incorrect!';
+       incorrect++;
+   }
+   questionIndex++;
+   produceQuestion();
+});
+
+choice_B.addEventListener('click', function(){
+   if (choice_B.innerHTML === allQuestions[questionIndex].Correct){
+       console.log('correct');
+       result.innerHTML = 'Correct!';
+       correct++;
+   }else{
+       result.innerHTML = 'Incorrect!'
+       incorrect++;
+   }
+   questionIndex++;
+   produceQuestion();
+});
+
+choice_C.addEventListener('click', function(){
+   if (choice_C.innerHTML === allQuestions[questionIndex].Correct){
+       console.log('correct');
+       result.innerHTML = 'Correct!';
+       correct++;
+   }else{
+       console.log('incorrect')
+       result.innerHTML = 'Incorrect!'
+       incorrect++;
+   }
+   questionIndex++;
+   produceQuestion();
+});
+
+choice_D.addEventListener('click', function(){
+   if (choice_D.innerHTML === allQuestions[questionIndex].Correct){
+       console.log('correct');
+       result.innerHTML = 'Correct!';
+       correct++;
+   }else{
+       result.innerHTML = 'Incorrect!'
+       incorrect++;
+   }
+   questionIndex++;
+   produceQuestion();
+});
 
 
 
- function quiz(){
-    for(var i=0; i<allQestions.length; i++){
-        var questionPrint = document.getElementById('question');
-         questionPrint.textContent((allQuestions[i])[0].question);
+
+
+//  function quiz(){
+//     for(var i=0; i<allQestions.length; i++){
+//         var questionPrint = document.getElementById('question');
+//          questionPrint.textContent((allQuestions[i])[0].question);
          
 
-        if(response === allQuestions[i].answer){
-            correct++;
-        }
-        else{incorrect++;}
-     }
- }
+//         if(response === allQuestions[i].answer){
+//             correct++;
+//         }
+//         else{incorrect++;}
+//      }
+//  }
 
